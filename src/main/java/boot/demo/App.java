2a0a389c65a28d2e891f76f79566c7bfb0f782ef
mario.liu <mario.liu@greaconsulting.com>
2019-07-09 00:45:05 +0800

    SpringBoot 主配置类及@SpringBootApplication注解的学习

--- src/main/java/boot/demo/App.java
+++ src/main/java/boot/demo/App.java
@@ -2,14 +2,31 @@ package boot.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Hello world!
+ * 配置类==配置文件，配置类也是容器中的一个组件 @Component
+ *
+ * @SpringBootApplication   :Springboot的主配置类，运行该类的main方法来启动springboot
+ *    --@Target({ElementType.TYPE})
+ *      @Retention(RetentionPolicy.RUNTIME)
+ *      @Documented
+ *      @Inherited
+ *      @SpringBootConfiguration    :springboot的配置类注解
+ *         ---@Configuration:配置类注解
+ *      @EnableAutoConfiguration    ：开启自动配置功能
+ *          ---@AutoConfigurationPackage  自动配置包，将主配置类 所在包及所有子包 里的所有组件扫描进去
+ *                  -----底层注解  @Import
+ *          ---@Import({AutoConfigurationImportSelector.class})     ：导入自动配置类（xxxAutoConfiguration）组件，以全类名的方式返回，将组件配置好并添加到容器中
+ *              通过SpringFactoriesLoader.loadFactoryNames(this.getSpringFactoriesLoaderFactoryClass(), this.getBeanClassLoader());
+ *                  加载类路径下的  拿到"META-INF/spring.factories"中EnableAutoConfiguration.class;指定的值
+ *
+ *                  J2EE的整体解决方案都在AutoConfiguration包中
+ *
+ *      @ComponentScan(excludeFilters = {@Filter(type = FilterType.CUSTOM,classes = {TypeExcludeFilter.class}),
+ *                                       @Filter(type = FilterType.CUSTOM,classes = {AutoConfigurationExcludeFilter.class})})
+ *
  *
  */
-@RestController
 @SpringBootApplication
 public class App 
 {
@@ -19,8 +36,5 @@ public class App
         SpringApplication.run(App.class, args);
     }
 
-    @GetMapping("/demo")
-    public String demo(){
-        return "hello spring boot demo~~~";
-    }
+
 }
